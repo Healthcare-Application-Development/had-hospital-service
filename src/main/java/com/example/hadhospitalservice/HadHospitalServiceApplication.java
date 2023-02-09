9ccda005e9483af96fe086bfc3576e064eccd961
rohitraman <rohitraman9944@gmail.com>
2023-02-09 23:28:35 +0530

    first commit

---
+++ src/main/java/com/example/hadhospitalservice/HadHospitalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.hadhospitalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadHospitalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HadHospitalServiceApplication.class, args);
+	}
+
+}
