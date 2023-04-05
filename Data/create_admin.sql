be76fe6568502dde248cb99595da31866fd31bb5
rohitraman <rohitraman9944@gmail.com>
2023-04-05 22:06:32 +0530

    Added Admin Dummy Data

---
+++ Data/create_admin.sql
@@ -0,0 +1,2 @@
+insert into login(username, password, role) values('rohit@yopmail.com', '$2a$10$InZX8Antitq5zznj65zfduTtHwwjDn/oNqQCP2xUHyPBuG.uUM2q.', 'ADMIN');
+insert into admin(name, phone_no, address, gender, email, login_id) values('R', '9898989898', '1A', 'M', 'rohit@yopmail.com', '1');
